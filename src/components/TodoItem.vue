1f67b52fd36cc8844fc0841eefdaf75cffe300ff
wagnercynthia774 <kigifolany@gmail.com>
2024-10-08 18:43:33 +0700

    Create TodoItem.vue

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <input 
+      type="text" 
+      :value="task.text" 
+      @input="$emit('editTask', $event.target.value)"
+    />
+    <button @click="$emit('deleteTask')">Delete</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    task: Object
+  }
+}
+</script>
+
+<style scoped>
+input {
+  border: none;
+  background-color: #333;
+  color: #fff;
+  padding: 5px;
+  margin-right: 10px;
+}
+
+input:focus {
+  outline: none;
+}
+
+button {
+  background-color: #ff5555;
+  color: white;
+  border: none;
+  padding: 5px 10px;
+  cursor: pointer;
+}
+
+button:hover {
+  background-color: #cc0000;
+}
+</style>
